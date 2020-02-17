fe892dd65f39a58fedb17191158cdff5fd17f4d8
lorena brito <lorenabrito051@outlook.com>
2020-02-17 15:28:36 -0300

    Adicionar arquivos de projeto.

---
+++ MostrarNomes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MostrarNomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nomes = new ArrayList();
+            string entrada, salario;
+
+            do
+            {
+                Console.WriteLine("Digite um nome");
+                entrada = Console.ReadLine();
+                Console.WriteLine("Digite o salário ou sair");
+                salario = Console.ReadLine();
+
+                if  (entrada == "sair") 
+                { 
+                    break; 
+                }
+                else 
+                {
+                    nomes.Add(entrada);
+                    nomes.Add(salario);
+                }                  
+            }while (true);
+
+            foreach (Object obj in nomes)
+            {
+                Console.WriteLine("{0}", obj);
+            }               
+
+            Console.ReadKey();
+        }
+    }
+}
